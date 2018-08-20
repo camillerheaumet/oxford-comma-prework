f685cde65508470799ecd38bee7ce26e1029fecf
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-20 15:11:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.length > 1
-    [array[0..-1].join(", "),array.last].join(" and ")
+    [array[0...-1].join(", "),array.last].join(" and ")
   else
     array
   end
